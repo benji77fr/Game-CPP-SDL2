984c9eb5f9af350ea67413cc645b5863bca0b233
Benjamin G <30464317+benji77fr@users.noreply.github.com>
2020-02-27 22:40:08 +0100

    Add keyboard controller

---
+++ Game.cpp
@@ -0,0 +1,98 @@
+#include "Game.hpp"
+#include "TextureManager.h"
+#include "Map.h"
+#include "Components.h"
+#include "Vector2D.h"
+
+Map* map;
+
+SDL_Renderer* Game::renderer = nullptr;
+SDL_Event Game::event;
+Manager manager;
+auto& player(manager.addEntity());
+
+Game::Game()
+{}
+Game::~Game()
+{}
+
+void Game::init(const char* title, int width, int height, bool fullscreen)
+{
+	int flags = 0;
+
+	if (fullscreen)
+	{
+		flags = SDL_WINDOW_FULLSCREEN;
+	}
+	
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	{
+		std::cout << "Subsystems Initialised!..." << std::endl;
+
+		window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, flags);
+
+		if (window)
+		{
+			std::cout << "Window Created!" << std::endl;
+		}
+
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		
+		if (renderer)
+		{	
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+			std::cout << "Renderer Created!" << std::endl;
+		}
+
+		isRunning = true;
+	}
+	else
+	{
+		isRunning = false;
+	}
+
+
+	map = new Map();
+
+	// ECS implementation
+	player.addComponent<TransformComponent>();
+	player.addComponent<SpriteComponent>("assets/player.png");
+	player.addComponent<KeyboardComponent>();
+
+
+}
+
+void Game::handleEvents()
+{
+
+	SDL_PollEvent(&event);
+
+	switch (event.type)
+	{
+	case SDL_QUIT:
+		isRunning = false;
+	default:
+		break;
+	}
+}
+
+void Game::update()
+{
+	manager.refresh();
+	manager.update();
+}
+
+void Game::render()
+{
+	SDL_RenderClear(renderer);
+	map->DrawMap();
+	manager.draw();
+	SDL_RenderPresent(renderer);
+}
+
+void Game::clean()
+{
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+}
